9970433954524e7620e5ba0f5b80e14563200fa0
Santiago Guevara <guevaras090@gmail.com>
2023-02-24 03:58:12 -0300

    Segundo commit

--- js/comprar.js
+++ js/comprar.js
@@ -8,62 +8,148 @@ class Products {
 }
 
 // Variable donde se almacenan
-let products = []
+let products = [];
 
 // Obtengo el carrito
-const cart = document.querySelector(".carrito-productos")
+const cart = document.querySelector(".carrito-productos");
+
+// Obtener productos del LS
+const getProducts = () => {
+    
+    // Obtengo el LS
+    let LSproducts = localStorage.getItem("products");
+
+    // Si no me devuelve null (que seria que hay algo) ejecuto el if
+    if (LSproducts !== null) {
+
+        // Parseo los objetos del JSON
+        const JSONproducts = JSON.parse(LSproducts);
+
+        // Recorro cada objeto y lo agrego nuevamente a la variable products utilizando la clase
+        products = JSONproducts.map(JSONproducts => new Products(JSONproducts.name, JSONproducts.price, JSONproducts.img));
+    }
+
+    // Retorno productos
+    return products;
+}
 
 const renderProducts = () => {
 
-    products.forEach(product => {
+    // Borro lo que hay (para no renderizar lo mismo)
+    cart.innerHTML = "";
+
+    // Todo lo que se ejecuta para agregar un producto (por eso el forEach)
+    products.forEach(product => {;
+        // Creacion de los elementos, ponerle las clases y algunos atributos
         const productDOM = document.createElement("div")
-        productDOM.className = "carrito-producto d-flex align-items-center"
+        productDOM.className = "carrito-producto d-flex align-items-center justify-content-between";
 
-        const img = document.createElement("img")
-        img.className = "carrito-producto__img"
-
-        const title = document.createElement("div")
-        title.className = "carrito-producto-titulo"
-
-        const price = document.createElement("div")
-        price.className = "carrito-producto-precio"
-
-        const smallTitle = document.createElement("small")
-        const smallPrice = document.createElement("small")
-
-        smallTitle.innerText = "Titulo"
-        smallPrice.innerText = "Precio"
-
-        const pTitle = document.createElement("p")
-        const pPrice = document.createElement("p")
-
-        pTitle.className = "fs-3"
-        pPrice.className = "fs-3"
-
-        pTitle.innerText = product.name
-        pPrice.innerText = product.price
-
-        title.append(smallTitle)
-        title.append(pTitle)
-
-        price.append(smallPrice)
-        price.append(pPrice)
-
-        productDOM.append(img)
-        productDOM.append(title)
-        productDOM.append(price)
+        const img = document.createElement("img");
+        img.className = `carrito-producto__img`;
+        img.setAttribute ("src", `${product.img}`);
 
 
-        console.log(productDOM)
+        const title = document.createElement("div");
+        title.className = "carrito-producto-titulo";
 
-        cart.append(productDOM)
+        const price = document.createElement("div");
+        price.className = "carrito-producto-precio";
+
+        const smallTitle = document.createElement("small");
+        const smallPrice = document.createElement("small");
+
+        smallTitle.innerText = "Producto";
+        smallPrice.innerText = "Precio";
+
+        const pTitle = document.createElement("p");
+        const pPrice = document.createElement("p");
+
+        pTitle.className = "fs-3";
+        pPrice.className = "fs-3";
+
+        pTitle.innerText = product.name;
+        pPrice.innerText = product.price;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "btn btn-danger";
+        deleteButton.innerText = `Eliminar ${product.name}`;
+
+        // Le agrego la funcionalidad al boton de eliminar
+        deleteButton.addEventListener("click", () => {
+            // Busco índice donde está el elemento en el array
+            const indexDelete = products.findIndex((productForDelete) => productForDelete.name === product.name);
+
+            // Borro el producto utilizando el índice del array obtenido en lo anterior
+            products.splice(indexDelete, 1);
+
+            updateLS();
+
+            // Renderizo el array nuevamente
+            renderProducts();
+        });
+
+        // Agrego todo al DOM
+        title.append(smallTitle);
+        title.append(pTitle);
+
+        price.append(smallPrice);
+        price.append(pPrice);
+
+        productDOM.append(img);
+        productDOM.append(title);
+        productDOM.append(price);
+        productDOM.append(deleteButton);
+
+        cart.append(productDOM);
     });
+
+    // Ejecuto la funcion de renderizar totales asi se actualiza simultaneamente con los productos
+    renderFinalPrices();
 }
+
+// Renderizar totales
+const renderFinalPrices = () => {
+    // Obtengo todo el div de los totales
+    const totals = document.querySelector (".totales")
+
+    // Si es 0 (lo que significa que no hay nada) que no se muestren los totales
+    if (products.length === 0) {
+        totals.className = "totales d-none"
+    } 
+    
+    // Filtro los productos en arrays por separado
+    const herb = products.filter ((product) => product.name.includes ("Yerba"))
+    const lightbulb = products.filter ((product) => product.name.includes ("Bombilla"))
+    const mate = products.filter ((product) => product.name.includes ("Mate"))
+
+    // Todos los valores, con sus respectiva operacion
+    const herbPrice = herb.reduce ((acumulador, productHerb) => acumulador + productHerb.price, 0)
+    const lightbulbPrice = lightbulb.reduce ((acumulador, productLightbulb) => acumulador + productLightbulb.price, 0)
+    const matePrice = mate.reduce ((acumulador, productmate) => acumulador + productmate.price, 0)
+    const iva = parseInt((herbPrice + lightbulbPrice + matePrice) * 0.21)
+    const total = herbPrice + lightbulbPrice + matePrice + iva
+
+    // Obtengo los p del DOM (Donde se almacenan los totales)
+    const pHerbPrice = document.querySelector (".precio-total-yerbas")
+    const pLightbulbPrice = document.querySelector (".precio-total-bombillas")
+    const pMatePrice = document.querySelector (".precio-total-mates")
+    const pIva = document.querySelector (".precio-total-iva")
+    const pTotal = document.querySelector (".precio-total-total")
+
+    // Les cambio los textos y les pongo los valores correspondientes
+    pHerbPrice.innerText = herbPrice
+    pLightbulbPrice.innerText = lightbulbPrice
+    pMatePrice.innerText = matePrice
+    pIva.innerText = iva
+    pTotal.innerText = total
+}
+
 
 // Actualizar LS
 const updateLS = () => {
-    localStorage.setItem("products", JSON.stringify(products))
+    localStorage.setItem("products", JSON.stringify(products));
 }
 
-getProducts()
-renderProducts()
+// Obtengo y renderizo los productos
+getProducts();
+renderProducts();
--- js/productos.js
+++ js/productos.js
@@ -1,8 +1,9 @@
 // Clase para construir productos
 class Products {
-    constructor(name, price) {
+    constructor(name, price, img) {
         this.name = name;
         this.price = price;
+        this.img = img;
     }
 }
 
@@ -10,27 +11,47 @@ class Products {
 let products = []
 
 // Obtengo todos los botones
-const herbTraditionalButton = document.querySelector(".button-1")
-const herbUruguayanButton = document.querySelector(".button-2")
-const herbOrganicButton = document.querySelector(".button-3")
-const herbBarbecueButton = document.querySelector(".button-4")
-const herbTerereButton = document.querySelector(".button-5")
-const herbSoftButton = document.querySelector(".button-6")
-const matePumpkinButton = document.querySelector(".button-7")
-const mateWoodButton = document.querySelector(".button-8")
-const mateAlpacaButton = document.querySelector(".button-9")
-const mateSilverButton = document.querySelector(".button-10")
-const mateSiliconeButton = document.querySelector(".button-11")
-const mateGlassButton = document.querySelector(".button-12")
-const lightbulbMetalButton = document.querySelector(".button-13")
-const lightbulbSteelButton = document.querySelector(".button-14")
-const lightbulbAlpacaButton = document.querySelector(".button-15")
-const lightbulbSilverButton = document.querySelector(".button-16")
-const lightbulbBronzeButton = document.querySelector(".button-17")
-const lightbulbGoldButton = document.querySelector(".button-18")
+const herbTraditionalButton = document.querySelector(".button-1");
+const herbUruguayanButton = document.querySelector(".button-2");
+const herbOrganicButton = document.querySelector(".button-3");
+const herbBarbecueButton = document.querySelector(".button-4");
+const herbTerereButton = document.querySelector(".button-5");
+const herbSoftButton = document.querySelector(".button-6");
+const matePumpkinButton = document.querySelector(".button-7");
+const mateWoodButton = document.querySelector(".button-8");
+const mateAlpacaButton = document.querySelector(".button-9");
+const mateSilverButton = document.querySelector(".button-10");
+const mateSiliconeButton = document.querySelector(".button-11");
+const mateGlassButton = document.querySelector(".button-12");
+const lightbulbMetalButton = document.querySelector(".button-13");
+const lightbulbSteelButton = document.querySelector(".button-14");
+const lightbulbAlpacaButton = document.querySelector(".button-15");
+const lightbulbSilverButton = document.querySelector(".button-16");
+const lightbulbBronzeButton = document.querySelector(".button-17");
+const lightbulbGoldButton = document.querySelector(".button-18");
 
 // Ventana del popup
-const added = document.querySelector("#added")
+const added = document.querySelector("#added");
+
+// Obtener productos del LS
+const getProducts = () => {
+    
+    // Obtengo el LS
+    let LSproducts = localStorage.getItem("products");
+
+    // Si no me devuelve null (que seria que hay algo) ejecuto el if
+    if (LSproducts !== null) {
+
+        // Parseo los objetos del JSON
+        const JSONproducts = JSON.parse(LSproducts);
+
+        // Recorro cada objeto y lo agrego nuevamente a la variable products utilizando la clase
+        products = JSONproducts.map(JSONproducts => new Products(JSONproducts.name, JSONproducts.price, JSONproducts.img));
+    }
+
+    // Retorno productos
+    return products;
+}
 
 // Mostrar popup con el nombre y el precio del producto
 const showPopup = (name, price) => {
@@ -38,7 +59,7 @@ const showPopup = (name, price) => {
     added.style.display = 'block';
 
     // Ponerle el texto al popup
-    added.innerHTML = `<p>Se agrego ${name} - ${price}</p>`
+    added.innerHTML = `<p>Se agrego ${name} - ${price}</p>`;
 
     // Ocultar la ventana emergente después de 2 segundos
     setTimeout(() => {
@@ -47,40 +68,41 @@ const showPopup = (name, price) => {
 }
 
 // Pushear el producto a la variable productos
-const pushProductosToTheVar = (name, price) => {
-    products.push(new Products(name, price))
-    updateLS()
+const pushProductosToTheVar = (name, price, img) => {
+    products.push(new Products(name, price, img));
+    updateLS();
 }
 
 // Pushear y mostrar el popup
-const pushProductosAndShowPopup = (name, price) => {
-    pushProductosToTheVar(name, price)
-    showPopup(name, price)
+const pushProductosAndShowPopup = (name, price, img) => {
+    pushProductosToTheVar(name, price, img);
+    showPopup(name, price);
 }
 
 // Actualizar LS
 const updateLS = () => {
-    localStorage.setItem("products", JSON.stringify(products))
+    localStorage.setItem("products", JSON.stringify(products));
 }
 
 // Botones con el esuchador de eventos, ejecutando la funcion de agregar y mostrar popup
-herbTraditionalButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba tradicional", 4321, "../imgs/yerba-tradicional.png"))
-herbUruguayanButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba uruguaya", 856, "../imgs/yerba-uruguaya.png"))
-herbOrganicButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba organica", 4213, "../imgs/yerba-organica.png"))
-herbBarbecueButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba barbacua", 3940, "../imgs/yerba-barbacua.png"))
-herbTerereButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba terere", 234, "../imgs/yerba-terere.png"))
-herbSoftButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba suave", 1645, "../imgs/yerba-suave.png"))
-matePumpkinButton.addEventListener("click", () => pushProductosAndShowPopup("Mate calabaza", 2345, "../imgs/yerba-tradicional.png")) // me quede aca
-mateWoodButton.addEventListener("click", () => pushProductosAndShowPopup("Mate madera", 452, "../imgs/yerba-tradicional.png"))
-mateAlpacaButton.addEventListener("click", () => pushProductosAndShowPopup("Mate alpaca", 6194, "../imgs/yerba-tradicional.png"))
-mateSilverButton.addEventListener("click", () => pushProductosAndShowPopup("Mate plata", 4296, "../imgs/yerba-tradicional.png"))
-mateSiliconeButton.addEventListener("click", () => pushProductosAndShowPopup("Mate silicona", 254, "../imgs/yerba-tradicional.png"))
-mateGlassButton.addEventListener("click", () => pushProductosAndShowPopup("Mate vidrio", 9123, "../imgs/yerba-tradicional.png"))
-lightbulbMetalButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla metal", 127, "../imgs/yerba-tradicional.png"))
-lightbulbSteelButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla acero", 7245, "../imgs/yerba-tradicional.png"))
-lightbulbAlpacaButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla alpaca", 1834, "../imgs/yerba-tradicional.png"))
-lightbulbSilverButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla plata", 274, "../imgs/yerba-tradicional.png"))
-lightbulbBronzeButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla bronce", 1920, "../imgs/yerba-tradicional.png"))
-lightbulbGoldButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla oro", 439, "../imgs/yerba-tradicional.png"))
+herbTraditionalButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba tradicional", 4321, "../imgs/yerba-tradicional.png"));
+herbUruguayanButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba uruguaya", 856, "../imgs/yerba-uruguaya.png"));
+herbOrganicButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba organica", 4213, "../imgs/yerba-organica.png"));
+herbBarbecueButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba barbacua", 3940, "../imgs/yerba-barbacua.png"));
+herbTerereButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba terere", 234, "../imgs/yerba-terere.png"));
+herbSoftButton.addEventListener("click", () => pushProductosAndShowPopup("Yerba suave", 1645, "../imgs/yerba-suave.png"));
+matePumpkinButton.addEventListener("click", () => pushProductosAndShowPopup("Mate calabaza", 2345, "../imgs/mate-calabaza.png")) ;
+mateWoodButton.addEventListener("click", () => pushProductosAndShowPopup("Mate madera", 452, "../imgs/mate-madera.png"));
+mateAlpacaButton.addEventListener("click", () => pushProductosAndShowPopup("Mate alpaca", 6194, "../imgs/mate-alpaca.png"));
+mateSilverButton.addEventListener("click", () => pushProductosAndShowPopup("Mate plata", 4296, "../imgs/mate-plata.png"));
+mateSiliconeButton.addEventListener("click", () => pushProductosAndShowPopup("Mate silicona", 254, "../imgs/mate-silicona.png"));
+mateGlassButton.addEventListener("click", () => pushProductosAndShowPopup("Mate vidrio", 9123, "../imgs/mate-vidrio.png"));
+lightbulbMetalButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla metal", 127, "../imgs/bombilla-metal.png"));
+lightbulbSteelButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla acero", 7245, "../imgs/bombilla-acero.png"));
+lightbulbAlpacaButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla alpaca", 1834, "../imgs/bombilla-alpaca.png"));
+lightbulbSilverButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla plata", 274, "../imgs/bombilla-plata.png"));
+lightbulbBronzeButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla bronce", 1920, "../imgs/bombilla-bronze.png"));
+lightbulbGoldButton.addEventListener("click", () => pushProductosAndShowPopup("Bombilla oro", 439, "../imgs/bombilla-oro.png"));
 
-getProducts()
+// Si hay productos en LS los obtengo
+getProducts();
